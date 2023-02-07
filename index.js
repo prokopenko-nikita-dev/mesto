ae29f257db56e1a8c8c9edd9dc5c05ce72f91602
NikitaProkopenko1 <prockopencko.nikit@yandex.ru>
2023-02-07 17:03:23 +0300

    1 version

--- index.js
+++ index.js
@@ -6,24 +6,26 @@ const btnEditProfile = document.querySelector(".profile__edit-profile");
 const btnEditProfileSave = document.querySelector(".popup__save-button");
 const cardsSection = document.querySelector(".elements");
 
+const userName = document.querySelector(".profile__edit-name");
+const userDescription = document.querySelector(".profile__edit-prof");
+
+const formUserName = document.querySelector(".popup__input_type_name");
+const formUserDescription = document.querySelector(".popup__input_type_prof");
+
 btnEditProfile.addEventListener("click", () => {
     popup.classList.add("popup_opened");
 
-    const userName = document.querySelector(".profile__edit-name").textContent;
-    const userDescription = document.querySelector(".profile__edit-prof").textContent;
-
-    const formUserName = document.querySelector(".popup__input_type_name");
-    const formUserDescription = document.querySelector(".popup__input_type_prof");
-
-    formUserName.value = userName;
-    formUserDescription.value = userDescription;
+    formUserName.value = userName.textContent;
+    formUserDescription.value = userDescription.textContent;
 })
 
-popupBtnClose.addEventListener("click", () => {
-    popup.classList.remove("popup_opened");
-  });
+popupBtnClose.addEventListener("click", closePopup);
 
 formEditProfile.addEventListener("submit", formEditProfileHandler);
+
+function closePopup () {
+    popup.classList.remove("popup_opened");
+}
 
 function formEditProfileHandler(e) {
     e.preventDefault();
@@ -34,5 +36,5 @@ function formEditProfileHandler(e) {
     document.querySelector(".profile__edit-name").textContent = formUserNameData;
     document.querySelector(".profile__edit-prof").textContent = formUserDescriptionData;
 
-    popup.classList.remove("popup_opened");
+    closePopup()
 }
